02fb33950d6013f02acc592d8c99730eb4ae2be0
David S-B <d.sentenac-bossiere@hotmail.fr>
2019-02-10 23:40:09 +0100

    First Commit

---
+++ src/components/TodoItems.js
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+import "./TodoItems.css";
+
+export class TodoItems extends Component {
+  getStyle = () => {
+    return {
+      textDecoration: this.props.todo.completed ? "line-through" : "none"
+    };
+  };
+
+  render() {
+    const { id, title } = this.props.todo;
+    return (
+      <div
+        style={{
+          background: "#f4f4f4",
+          padding: "10px",
+          border: "2px black dotted",
+          borderRadius: "7px",
+          margin: "5px"
+        }}
+      >
+        <p>
+          <span>
+            <input
+              type="checkbox"
+              onChange={this.props.markComplete.bind(this, id)}
+            />{" "}
+            {""}
+          </span>
+          <span style={this.getStyle()}>{title}</span>
+          <button style={btnStyle} onClick={this.props.delTodo.bind(this, id)}>
+            <span role="img" aria-label="redCross">
+              ❌
+            </span>
+          </button>
+        </p>
+      </div>
+    );
+  }
+}
+
+const btnStyle = {
+  border: "none",
+  backgroundColor: "#f4f4f4",
+  float: "right",
+  cursor: "pointer"
+};
+
+export default TodoItems;
---
+++ src/components/layout/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <h1 style={{ margin: "20px", color: "white" }}>To-Do list</h1>
+    </header>
+  );
+};
+
+export default Header;
